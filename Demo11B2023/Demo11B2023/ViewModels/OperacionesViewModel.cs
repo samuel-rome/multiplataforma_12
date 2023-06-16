a5524926838fe9a7c07d716c0418d3787c0b4ae0
Samuel <samuel2004romero@gmail.com>
2023-06-16 16:37:53 -0500

    first commit

---
+++ Demo11B2023/Demo11B2023/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Demo11B2023.ViewModels
+{
+    public class OperacionesViewModel : ViewModelBase
+    {
+
+        int b;
+        public int Base
+        {
+            get { return b; }
+            set
+            {
+                if (b != value)
+                {
+                    b = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        int h;
+        public int Altura
+        {
+            get { return h; }
+            set
+            {
+                if (h != value)
+                {
+                    h = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int area;
+        public int Area
+        {
+            get { return area; }
+            set
+            {
+                if (area != value)
+                {
+                    area = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int perimetro;
+        public int Perimetro
+        {
+            get { return perimetro; }
+            set
+            {
+                if (perimetro != value)
+                {
+                    perimetro = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public ICommand CalcularArea { get; set; }
+        public ICommand CalcularPerimetro { get; set; }
+
+
+        public OperacionesViewModel()
+        {
+            CalcularArea = new Command(() =>
+            {
+                Area = b * h;
+            });
+
+            CalcularPerimetro = new Command(() =>
+            {
+                Perimetro = (b+h)*2;
+            });
+        }
+
+    }
+}
---
+++ Demo11B2023/Demo11B2023/Views/Operaciones.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo11B2023.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operaciones : ContentPage
+    {
+        public Operaciones()
+        {
+            InitializeComponent();
+            this.BindingContext= new ViewModels.OperacionesViewModel();
+
+
+
+        }
+    }
+}
